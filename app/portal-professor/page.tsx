7fffe41e271dfa57c1d63a1b88401c71dffbb696
Caique <caiquemorales20@gmail.com>
2023-09-03 13:55:09 -0300

    :heavy_plus_sign: build: Tremor

--- app/portal-professor/page.tsx
+++ app/portal-professor/page.tsx
@@ -1,7 +1,13 @@
+// Imported Components
+import BarChartS from '@/components/BarChart'
 import Container from '@/components/Container'
 
 // Functional Component
 export default function Portal() {
   // Rendering
-  return <Container>Portal</Container>
+  return (
+    <Container>
+      <BarChartS />
+    </Container>
+  )
 }
